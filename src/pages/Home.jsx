05c07d7a930b95d150b3f5a6c58d16c2cb346b1b
programmerkim <jenniferkim.businessonly@gmail.com>
2020-04-14 12:01:03 -0700

    routes fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,7 @@
 import React from "react";
-import Header from "../components/Navbar";
-import Footer from "../components/Footer";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 import "./style.css";
-import NavBar from "../components/Navbar";
 
 function Home() {
     return (
