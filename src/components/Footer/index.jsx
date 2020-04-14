cd7c0b9a0d2a614134619772325d7a2b845823cb
programmerkim <jenniferkim.businessonly@gmail.com>
2020-04-14 11:55:12 -0700

    footer comp

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../style.css";
+
+function Footer() {
+    return (
+        <footer class="footer mt-auto py-3">
+            <span class="text">Thank you for viewing Jennifer Kim's webpage, have a great
+                            day! Email: @jenniferkim.businessonly@gmail.com Phone: (253)-320-1709.</span>
+        </footer>
+    )
+};
+
+export default Footer;
