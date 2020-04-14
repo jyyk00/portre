5a646fd47ba08dff38c71c92fbcb6dfb12b2db85
programmerkim <jenniferkim.businessonly@gmail.com>
2020-04-14 11:59:47 -0700

    home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../components/Navbar";
+import Footer from "../components/Footer";
+import "./style.css";
+import NavBar from "../components/Navbar";
+
+function Home() {
+    return (
+        <div>
+            <NavBar />
+            <Footer />
+        </div>
+    )
+}
+
+export default Home;
